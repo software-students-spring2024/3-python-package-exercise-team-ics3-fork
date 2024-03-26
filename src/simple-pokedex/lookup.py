d5194210fc1ccc2483cf2c1273af79d85e7b0575
Justin Zhao <justin@justinzhao.net>
2024-03-26 19:32:58 -0400

    added pokemon lookup

--- src/simple-pokedex/lookup.py
+++ src/simple-pokedex/lookup.py
@@ -1,5 +1,20 @@
 import requests
 
 
-def lookup():
-    return {}
+def pokemonLookup(pokemon):
+    response = requests.get("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "success": True,
+            "message": "success",
+            "data": {
+                "name": data["name"],
+                "id": data["id"],
+                "types": [type["type"]["name"].upper() for type in data["types"]],
+                "stats": data["stats"],
+                "abilities": data["abilities"],
+            },
+        }
+    else:
+        return {"success": False, "message": response.status_code, "data": None}
