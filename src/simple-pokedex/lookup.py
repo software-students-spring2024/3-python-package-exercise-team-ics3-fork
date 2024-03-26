a4b2da57adb3aa511bc5c81bb7205b8c29db3a6e
Justin Zhao <justin@justinzhao.net>
2024-03-26 19:40:01 -0400

    added ability lookup

---
+++ src/simple-pokedex/lookup.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def pokemonLookup(pokemon):
+    response = requests.get("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "success": True,
+            "message": "success",
+            "data": {
+                "name": data["name"],
+                "id": data["id"],
+                "types": [type["type"]["name"].upper() for type in data["types"]],
+                "stats": data["stats"],
+                "abilities": data["abilities"],
+            },
+        }
+    else:
+        return {"success": False, "message": response.status_code, "data": None}
+
+
+def abilityLookup(ability):
+    response = requests.get("https://pokeapi.co/api/v2/ability/" + ability)
+    data = response.json()
+
+    # parse data
+    parsed_data = ""
+    for item in data["effect_entries"]:
+        if item["language"]["name"] == "en":
+            parsed_data = item["effect"]
+
+    if response.status_code == 200:
+        return {"success": True, "message": "success", "data": parsed_data}
+    else:
+        return {"success": False, "message": response.status_code, "data": None}
