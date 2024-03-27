96354bc7924593471de7ee738be7d4b541fdb6e7
Justin Zhao <justin@justinzhao.net>
2024-03-26 21:08:00 -0400

    create example file

---
+++ simple-pokedex/example/example.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.insert(1, "./simple-pokedex")
+
+import lookup
+import generatePokemon
+import poketypes
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
